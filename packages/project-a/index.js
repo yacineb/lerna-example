6bf32432e46644df768d880b83d462c83175ff6d
Jay Kariesch <jay@curbee.com>
2021-08-18 20:43:48 -0500

    feat: custom changelog
    add custom changelog
    
    [CH4997]

--- packages/project-a/index.js
+++ packages/project-a/index.js
@@ -2,7 +2,7 @@
 
 const _ = require('lodash');
 module.exports = function removeDupl(data) {
-    console.log('boof...')
+    console.log('booddf...')
     return _.uniqBy(data, function (e) {
         return e;
       });
