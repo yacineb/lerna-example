675b4e05ac854d7827cebcc43c3280c6e7e67dcd
Jay Kariesch <jay@curbee.com>
2021-08-18 21:52:55 -0500

    fix: new fix
    this was a really small fix in order to test conventional changelog. Did it work?
    
    [ch4555]

--- packages/project-a/index.js
+++ packages/project-a/index.js
@@ -2,7 +2,7 @@
 
 const _ = require('lodash');
 module.exports = function removeDupl(data) {
-    console.log('shit')
+    console.log('giving this a shot')
     return _.uniqBy(data, function (e) {
         return e;
       });
