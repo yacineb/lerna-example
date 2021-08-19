bab6ab3a933f5eb8eec7bdaedf60477f8ba01c60
Jay Kariesch <jay@curbee.com>
2021-08-18 22:50:24 -0500

    fix: helium yay
    oh hey mri helium
    
    [ch4995]

--- packages/project-a/index.js
+++ packages/project-a/index.js
@@ -2,7 +2,7 @@
 
 const _ = require('lodash');
 module.exports = function removeDupl(data) {
-    console.log('pleazzeee cheessee')
+    console.log('to cheessee')
     return _.uniqBy(data, function (e) {
         return e;
       });
