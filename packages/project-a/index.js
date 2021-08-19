181652318b533134c665326121ee5fea10de03b3
Jay Kariesch <jay@curbee.com>
2021-08-18 20:14:54 -0500

    feat: added oof
    Added a great big oof to the app
    
    [CH4762]

--- packages/project-a/index.js
+++ packages/project-a/index.js
@@ -2,7 +2,7 @@
 
 const _ = require('lodash');
 module.exports = function removeDupl(data) {
-    console.log('dougy...')
+    console.log('oof...')
     return _.uniqBy(data, function (e) {
         return e;
       });
