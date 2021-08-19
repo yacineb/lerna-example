1ed502e403537104ecfa7aee7f05cfd052e9fbb9
Jay Kariesch <jay@curbee.com>
2021-08-18 20:41:31 -0500

    feat: custom changelog
    add custom changelog
    
    [CH4997]

---
+++ changelog-preset.js
@@ -0,0 +1,12 @@
+const config = require('conventional-changelog-conventionalcommits');
+const { resolve } = require('path')
+
+module.exports =  Promise.resolve()
+.then(() => require('conventional-changelog-angular'))
+.then(presetPromise => presetPromise())
+.then(preset => {
+    preset.issuePrefixes = ["ch", "CH"]
+    preset.writerOpts.headerPartial = resolve("./header.hbs")
+    preset.issueUrlFormat = "https://app.clubhouse.io/curbee/story/{{id}}"
+    return preset
+})
